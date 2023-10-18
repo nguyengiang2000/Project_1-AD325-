4052cb1609852612ab3629fe5be1d984e2b6d9e1
Nguyen Giang <giangthuynguyen2000@gmail.com>
2023-10-18 12:13:49 -0700

    Add StockLedge and StockPurchase

---
+++ src/StockLedger/StockLedge.java
@@ -0,0 +1,85 @@
+package Deque.StockLedger;
+
+
+import java.util.ArrayList;
+
+
+public class StockLedger implements StockLedgerInterface {
+
+
+    private ArrayList<StockPurchase> ledger;
+
+
+
+
+    public StockLedger(){
+        ledger = new ArrayList<>();
+    }
+    /**
+     * Records a stock purchase in this ledger.
+     *
+     * @param stockSymbol   The stock's symbol.
+     * @param sharesBought  The number of shares purchased.
+     * @param pricePerShare The price per share.
+     */
+    @Override
+    public void buy(String stockSymbol, int sharesBought, double pricePerShare) {
+        StockPurchase purchase = new StockPurchase(stockSymbol, sharesBought, pricePerShare);
+        ledger.add(purchase);
+    }
+
+
+    /**
+     * Removes from this ledger any shares of a particular stock
+     * that were sold and computes the capital gain or loss.
+     *
+     * @param stockSymbol   The stock's symbol.
+     * @param sharesSold    The number of shares sold.
+     * @param pricePerShare The price per share.
+     * @return The capital gain (loss).
+     */
+    @Override
+    public double sell(String stockSymbol, int sharesSold, double pricePerShare) {
+        double saleAmount = sharesSold * pricePerShare;
+        double totalCost = 0;
+
+
+        return 0;
+    }
+
+
+    /**
+     * Returns a boolean on whether the passed in stock symbol is contained in the ledger.
+     *
+     * @param stockSymbol The stock's symbol.
+     * @return Boolean of if the stock exists in the ledger.
+     */
+    @Override
+    public boolean contains(String stockSymbol) {
+        while(!ledger.isEmpty()){
+            if(ledger.contains(stockSymbol)){
+                System.out.println(stockSymbol + " is exist !");
+                return true;
+            }
+            else {
+                System.out.println(stockSymbol + " is not exist !");
+                return false;
+            }
+        }
+        System.out.println(" The ledge is empty! ");
+        return false;
+    }
+
+
+    /**
+     * Returns a LedgerEntry object based on stock symbol.
+     *
+     * @param stockSymbol The stock's symbol.
+     * @return LedgerEntry object of stock symbol.
+     */
+    @Override
+    public LedgerEntry getEntry(String stockSymbol) {
+        return null;
+    }
+}
+
---
+++ src/StockLedger/StockPurchase.java
@@ -0,0 +1,34 @@
+package Deque.StockLedger;
+
+public class StockPurchase {
+    private String stockSymbol;
+    private int shareBought;
+
+    private double pricePerShare;
+
+    /** stockPurchase constructor with 3 elements
+     *
+     * @param stockSymbol => stock symbol
+     * @param shareBought => how many share was in transaction
+     * @param pricePerShare => how much per share cost
+     */
+    public StockPurchase(String stockSymbol, int shareBought, double pricePerShare){
+        this.stockSymbol = stockSymbol;
+        this.shareBought = shareBought;
+        this.pricePerShare = pricePerShare;
+    }
+
+    public String getStockSymbol(){
+        return stockSymbol;
+    }
+
+    public int getShareBought(){
+        return shareBought;
+    }
+
+    public double getPricePerShare(){
+        return pricePerShare;
+    }
+
+}
+
