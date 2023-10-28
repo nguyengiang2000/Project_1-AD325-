46b5a3149443e8bdca2f7afd5a8b1c1a9d74eb00
Nguyen Giang <giangthuynguyen2000@gmail.com>
2023-10-28 10:01:02 -0700

    update variable transaction in main

--- src/StockLedger/main.java
+++ src/StockLedger/main.java
@@ -2,30 +2,30 @@ package Deque.StockLedger;
 
 public class main {
     public static void main(String[] args) {
-        StockLedger transaction_1 = new StockLedger();
-        transaction_1.buy("AAPL", 20, 45);
-        transaction_1.buy("AAPL", 20, 75);
-        transaction_1.buy("MSFT", 20, 95);
-        transaction_1.Diplay();
-        transaction_1.sell("AAPL", 30, 65);
-        transaction_1.Diplay();
-        transaction_1.sell("AAPL", 10, 65);
-        transaction_1.Diplay();
-        transaction_1.buy("AAPL", 100, 20);
-        transaction_1.buy("AAPL", 20, 24);
-        transaction_1.buy("TSLA", 200, 36);
-        transaction_1.Diplay();
-        transaction_1.sell("AAPL", 10, 65);
-        transaction_1.Diplay();
-        transaction_1.sell("TSLA", 150, 30);
-        transaction_1.Diplay();
-        transaction_1.buy("MSFT", 5, 60);
-        transaction_1.buy("MSFT", 5, 70);
-        transaction_1.Diplay();;
-        transaction_1.sell("MSFT", 4, 30);
-        transaction_1.Diplay();
-        transaction_1.sell("MSFT", 2, 30);
-        transaction_1.Diplay();
+        StockLedger transaction = new StockLedger();
+        transaction.buy("AAPL", 20, 45);
+        transaction.buy("AAPL", 20, 75);
+        transaction.buy("MSFT", 20, 95);
+        transaction.Diplay();
+        transaction.sell("AAPL", 30, 65);
+        transaction.Diplay();
+        transaction.sell("AAPL", 10, 65);
+        transaction.Diplay();
+        transaction.buy("AAPL", 100, 20);
+        transaction.buy("AAPL", 20, 24);
+        transaction.buy("TSLA", 200, 36);
+        transaction.Diplay();
+        transaction.sell("AAPL", 10, 65);
+        transaction.Diplay();
+        transaction.sell("TSLA", 150, 30);
+        transaction.Diplay();
+        transaction.buy("MSFT", 5, 60);
+        transaction.buy("MSFT", 5, 70);
+        transaction.Diplay();;
+        transaction.sell("MSFT", 4, 30);
+        transaction.Diplay();
+        transaction.sell("MSFT", 2, 30);
+        transaction.Diplay();
 
     }
 }
